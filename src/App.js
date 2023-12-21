bba706fb5ca7cd1145e0764b284e537ce362646e
AndreyOvchynnikov <andrey272015@gmail.com>
2023-12-21 10:38:21 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { Route, Routes } from "react-router-dom";
 import { AuthContext } from "./components/context/authContext";
 import "../src/index.css";
 
-const SharedLayout = lazy(() => import('./components/SharedLayout'));
-const About = lazy(() => import('./pages/About'));
-const Home = lazy(() => import('./pages/Home'));
-const ModelsPage = lazy(() => import('./pages/ModelsPage'));
-const TestimonialsPage = lazy(() => import('./pages/TestimonialsPage'));
-const TeamPage = lazy(() => import('./pages/TeamPage'));
-// const ContactsPage = lazy(() => import('./pages/ContactsPage'));
+const SharedLayout = lazy(() => import("./components/SharedLayout"));
+const About = lazy(() => import("./pages/About"));
+const Home = lazy(() => import("./pages/Home"));
+const ModelsPage = lazy(() => import("./pages/ModelsPage"));
+const TestimonialsPage = lazy(() => import("./pages/TestimonialsPage"));
+const TeamPage = lazy(() => import("./pages/TeamPage"));
+const ContactsPage = lazy(() => import("./pages/ContactsPage"));
 
 
 const App = () => {
@@ -32,7 +32,7 @@ function appHeight() {
           <Route path="models" element={<ModelsPage />} />
           <Route path="testimonials" element={<TestimonialsPage />} />
           <Route path="team" element={<TeamPage />} />
-          {/* <Route path="contact" element={<ContactsPage />} /> */}
+          <Route path="contact" element={<ContactsPage />} />
         </Route>
       </Routes>
     </AuthContext>
